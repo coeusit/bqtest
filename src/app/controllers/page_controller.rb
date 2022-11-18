9975f972f638e2505603dc1f71b73fb68e930453
Melvin Sommer <melvin@coeus-it.com>
2022-11-18 12:29:42 +0100

    up

--- src/app/controllers/page_controller.rb
+++ src/app/controllers/page_controller.rb
@@ -1,4 +1,12 @@
 class PageController < ApplicationController
   def index
+    @companies = []
+    if params[:query].present?
+      _query = "%#{params[:query]}%"
+      @companies = Company.where("registry_number LIKE ? OR name LIKE ? OR city LIKE ?", _query, _query, _query)
+      if turbo_frame_request?
+        render partial: 'page/search_results', locals: { companies: @companies }
+      end
+    end
   end
 end
